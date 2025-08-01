3987304323e2df5f0b7014e43d40bc68ea6f4dc0
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-08-01 21:51:36 +0000

    Update Invite.js

--- models/Invite.js
+++ models/Invite.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
 const inviteSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
+  userId: { type: String, required: true, unique: true },
   inviteLink: { type: String, required: true },
-  messageId: { type: Number, required: true }, // Ajouté
-  chatId: { type: Number, required: true },    // Ajouté
-  createdAt: { type: Date, default: Date.now }
+  chatId: { type: Number, required: true },
+  messageId: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
+  expiresAt: { type: Date, required: true } // → date d’expiration du lien
 });
 
 export default mongoose.model('Invite', inviteSchema);
