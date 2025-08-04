5bfbf79cb5c47ff0df190320e351242a725910f1
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-08-04 14:40:22 +0000

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,7 +1,12 @@
+// models/Admin.js
 import mongoose from 'mongoose';
 
 const AdminSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true }
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  }
 });
 
 export default mongoose.model('Admin', AdminSchema);
